5784ceeaa204879d99fe220323bc0a909bb68280
Kaushik Sambamurthy <kaushik.s02@gmail.com>
2020-02-22 14:26:25 -0500

    CPU utilization WIP
    - Fixed error in computing process uptime

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -258,7 +258,7 @@ long LinuxParser::UpTime(int pid) {
     std::getline(fileStream, line);
     std::istringstream lineStream(line);
     std::vector<std::string> data{std::istream_iterator<string>{lineStream}, std::istream_iterator<string>{}};
-    return std::stol(data[21]);
+    return LinuxParser::UpTime() - std::stol(data[21]);
   }
   return 0;
 }
--- src/process.cpp
+++ src/process.cpp
@@ -18,7 +18,8 @@ int Process::Pid() { return pid_; }
 // TODO: Return this process's CPU utilization
 // Returns a value between 0 and 1.
 float Process::CpuUtilization() { 
-    long systemUpTime = LinuxParser::UpTime();
+    // long systemUpTime = LinuxParser::UpTime();
+    // float sysUpTimeSeconds = systemUpTime / hertz_;
     std::vector<long int> processInfo = LinuxParser::Cpu(pid_);
     enum processInfoElem {
         utime_ = 0, 
@@ -29,8 +30,10 @@ float Process::CpuUtilization() {
     };
     long int total_time = processInfo[processInfoElem::utime_] + processInfo[processInfoElem::stime_];
     total_time += processInfo[processInfoElem::cutime_] + processInfo[processInfoElem::cstime_];
-    float seconds = systemUpTime - (processInfo[processInfoElem::starttime_] / hertz_);
-    float cpu_usage = 100 * ((total_time / hertz_) / seconds);
+    // float seconds = sysUpTimeSeconds - (processInfo[processInfoElem::starttime_] / hertz_);
+    // float processUpTimeSeconds = Process::UpTime();
+    float seconds = (float) Process::UpTime();
+    float cpu_usage = ((total_time / hertz_) / seconds);
     return cpu_usage; 
 }
 
